7af3667bff2139b430edd67f435f4ffc2a1cf3bb
TasiYokan <guanghao.yang@outlook.com>
2017-12-22 14:40:41 -0500

    Add PrototypeToolkit dll and test

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TasiYokan.Audio;
+using TasiYokan.EventsInUnity;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        NotificationCenter.Instance.AddEventListener<BaseEvent>("Ah", (_sender, _evt) =>
+        {
+            int a = 1;
+            print("Ohhhhh");
+        });
+        print("Ah");
+        NotificationCenter.Instance.DispatchEvent("Ah", new BaseEvent());
+
+        float b = 2.2f;
+        b.Sgn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
